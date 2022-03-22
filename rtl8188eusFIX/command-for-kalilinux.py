bead25367912aa79df4a1a568a1ed26016fe0d47
HEVIN <93322212+sonvan1811@users.noreply.github.com>
2022-03-22 15:27:54 +0700

    Update command-for-kalilinux.py

--- rtl8188eusFIX/command-for-kalilinux.py
+++ rtl8188eusFIX/command-for-kalilinux.py
@@ -6,6 +6,7 @@ command2 = 'echo "blacklist r8188eu" > "/etc/modprobe.d/realtek.conf"'
 command3 = 'exit'
 command4 = input("[sudo] Enter your password: ")
 
+pyautogui.hotkey("ctrl", "shift", "n")
 
 pyperclip.copy(command1)
 pyautogui.hotkey("ctrl", "shift", "v")
